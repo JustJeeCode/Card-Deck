d9ccbb2b24c7229cbd5d6296fdeb1cc0145e52ae
Jee <giorpieri@gmail.com>
2020-01-14 13:56:15 +1000

    Starting code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 "# Card-Deck" 
+
+This Repo is a simple card deck shuffler simulator.
+This was just a quick side project while working on the JARVIS discord bot.
+
+Made by JustJeeCode!
---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Deck Of Cards
+by JustJeeCode.
+
+Todo List:
+- Make the deck of cards
+    - 3 types in a card (suit, number, colour)
+- Make a function where you can shuffle them
+- Make it so you can draw a random card
+"""
+
+def main():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
