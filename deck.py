899680acfc59478d77f762c9ac0a1964cda43e77
Jee <giorpieri@gmail.com>
2020-01-14 16:49:31 +1000

    Starting the card deck code

--- deck.py
+++ deck.py
@@ -1,4 +1,38 @@
 """
 Classes:
+- Suit
+- Colour
+- Number
+- Misc
+"""
 
 """
+class Deck:
+    def __init__(self, card_suit, card_number, card_colour):
+        self.card_suit = card_suit
+        self.card_number = card_number
+        self.card_colour = card_colour
+"""
+
+class Suit:
+    def __init__(self, clubs, diamonds, hearts, spades):
+        self.clubs = clubs
+        self.diamonds = diamonds
+        self.hearts = hearts
+        self.spades = spades
+
+class Number:
+    def __init__(self, card_numb):
+        self.card_numb = card_numb
+
+
+class Colour:
+    def __init__(self, black, red):
+        self.black = black
+        self.red = red
+
+class Misc:
+    def __init__(self, jack, king, queen):
+        self.jack = jack
+        self.king = king
+        self.queen = queen
--- main.py
+++ main.py
@@ -6,11 +6,28 @@ Todo List:
 - Make the deck of cards
     - 3 types in a card (suit, number, colour)
 - Make a function where you can shuffle them
-- Make it so you can draw a random card
 """
+from deck import Suit
+from deck import Number
+from deck import Colour
+
 
 def main():
-    print("Hello World")
+    #Card Deck
+    #Numbers
+    Number.card_numb = ["ace ", 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
+
+    #Colours
+    Colour.red = "red"
+    Colour.black = "black"
+
+    #Suits
+    Suit.clubs = [Number.card_numb[0] + Colour.black + " ♣️"]
+    Suit.diamonds = []
+    Suit.hearts = []
+    Suit.spades = []
+
+    print(Suit.clubs)
 
 if __name__ == "__main__":
-    main()
+    main() 
